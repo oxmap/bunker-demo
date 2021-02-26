96139bb666528e6f42e38df78a095bdd611ce6a9
SavinAO <SavinAO@tomskasu.ru>
2021-02-26 13:48:48 +0700

    Добавлены дополнительные проверки

--- public/main.js
+++ public/main.js
@@ -9,6 +9,7 @@ function setClass() {
 }
 
 setClass();
+enableDemo('demo-info');
 
 window.addEventListener(
   "resize",
@@ -17,3 +18,37 @@ window.addEventListener(
   },
   true
 );
+
+function enableDemo(id) {
+  disableDemo(id);
+  document.querySelector(`#${id}`).style.display = 'block';
+}
+
+function disableDemo() {
+  const ids = document.querySelectorAll('[id^=demo]')
+  Array.prototype.forEach.call( ids, function( el ) {
+    el.style.display = 'none';
+  });
+}
+
+// Slider
+setSlider('widthRange', 'widthInput');
+setSlider('heightRange', 'heightInput');
+
+function setSlider(rangeId, outputId) {
+  const slider = document.getElementById(rangeId);
+  const output = document.getElementById(outputId);
+  const id = rangeId.replace('Range', ''); 
+  output.value = slider.value; 
+  const size = `inner${id.charAt(0).toUpperCase() + id.slice(1)}`;
+  slider.value = window[size]; 
+  output.value = window[size];
+  output.oninput = function() {
+    slider.value = this.value;
+    document.querySelector('body').style[id] = `${this.value}px`;
+  }
+  slider.oninput = function() {
+    output.value = this.value;
+    document.querySelector('body').style[id] = `${this.value}px`;
+  }
+}
